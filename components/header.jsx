7a67eb156a728f98e1cd3bbf96f99babbf0831a9
gilionario <gil8couto@gmail.com>
2025-09-02 16:10:49 +0400

    Update header.jsx

--- components/header.jsx
+++ components/header.jsx
@@ -18,14 +18,17 @@ export function Header() {
                 <ul className="flex flex-wrap gap-x-4 gap-y-1">
                     {navItems.map((item, index) => (
                         <li key={index}>
-                            <Link href={item.href} className="inline-flex px-1.5 py-1 sm:px-3 sm:py-2">
+                            <Link
+                                href={item.href}
+                                className="inline-flex px-1.5 py-1 sm:px-3 sm:py-2 font-bold uppercase no-underline hover:text-green-400"
+                            >
                                 {item.linkText}
                             </Link>
                         </li>
                     ))}
                 </ul>
             )}
-            
         </nav>
     );
 }
+
