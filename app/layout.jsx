617cea642182e5e2e5116874ebf69c3402536f45
gilionario <gil8couto@gmail.com>
2025-09-02 15:04:25 +0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/enigmatelogo.png" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600">
+            <body className="antialiased text-white bg-gradient-to-r from-green-700 to-green-500">
                 <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
