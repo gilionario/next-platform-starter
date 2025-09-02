a415f0007931b6972f448f082a3cba7b104a3161
gilionario <gil8couto@gmail.com>
2025-09-02 14:48:53 +0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
             <head>
                 <link rel="icon" href="/enigmatelogo.png" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
+            <body className="antialiased text-white bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600">
                 <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
@@ -27,5 +27,6 @@ export default function RootLayout({ children }) {
         </html>
     );
 }
+
 
 
