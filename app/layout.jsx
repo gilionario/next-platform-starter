dfa894a96b80d28aa135306ca86267b48906d3d0
gilionario <gil8couto@gmail.com>
2025-09-02 14:02:03 +0400

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,29 +1,29 @@
 import '../styles/globals.css';
-import { Footer } from '../components/footer';
 import { Header } from '../components/header';
 
 export const metadata = {
     title: {
-        template: '%s | Netlify',
-        default: 'Netlify Starter'
+        template: '%s', // dynamic page title
+        default: 'Enigmate' // default site title
     }
 };
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="pt">
             <head>
                 <link rel="icon" href="/favicon.svg" sizes="any" />
             </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-noise sm:px-12">
+            <body className="antialiased text-white bg-gradient-to-r from-green-700 to-green-500">
+                <div className="flex flex-col min-h-screen px-6 sm:px-12">
                     <div className="flex flex-col w-full max-w-5xl mx-auto grow">
                         <Header />
                         <main className="grow">{children}</main>
-                        <Footer />
+                        {/* Footer removed */}
                     </div>
                 </div>
             </body>
         </html>
     );
 }
+
